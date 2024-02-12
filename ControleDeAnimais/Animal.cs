d3c1e755f3f95623074cc8d62a13bae6e30a3862
RodrLucas <lucasrodriguessantos0@gmail.com>
2024-02-12 01:23:53 -0300

    Adicionar arquivos de projeto.

---
+++ ControleDeAnimais/Animal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeAnimais
+{
+    public class Animal
+    {
+		//Estamos fazendo dessa forma para aplicar o conceito de encapsulamento da POO
+		//Dado privado -> É comum na comunidade, usar os nomes das propriedades privadas começando com o underline.
+		private string _nome;
+
+		//Propriedade pública
+		public string Nome
+		{
+			get { return this._nome; }
+			set { this._nome = value.ToUpper(); }
+		}
+
+		private string _tipo;
+
+		public string Tipo
+		{ // Valores definidos pelo proprietário - Cachorro, Gato, Peixe
+			get { return this._tipo; }
+			set 
+			{
+				var nomeAnimal = value.ToUpper();
+				switch (nomeAnimal)
+				{
+					case "CACHORRO": this._tipo = nomeAnimal; break;
+					case "GATO": this._tipo = nomeAnimal; break;
+					case "PEIXE": this._tipo = nomeAnimal; break;
+					default: this._tipo = "Peixe"; break;
+				}
+			}
+		}
+	}
+}
---
+++ ControleDeAnimais/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeAnimais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstAnimal = new Animal();
+            var secondAnimal = new Animal();
+            var thirdAnimal = new Animal();
+            var fourthAnimal = new Animal();
+            var fifthAnimal = new Animal();
+
+            int qtdCachorro = 0, qtdGato = 0, qtdPeixe = 0;
+
+
+            Console.WriteLine("Controle de aniamais");
+
+            //Primeiro
+            Console.Write("Informe o nome do primeiro animal: ");
+            firstAnimal.Nome = Console.ReadLine();
+
+            Console.Write("Informe o tipo do primeiro animal: ");
+            firstAnimal.Tipo = Console.ReadLine();
+
+            if(firstAnimal.Tipo.ToUpper() == "CACHORRO") qtdCachorro++;
+            if (firstAnimal.Tipo.ToUpper() == "GATO") qtdGato++;
+            if (firstAnimal.Tipo.ToUpper() == "PEIXE") qtdPeixe++;
+
+            //Segundo
+            Console.Write("Informe o nome do segundo animal: ");
+            secondAnimal.Nome = Console.ReadLine();
+
+            Console.Write("Informe o tipo do segundo animal: ");
+            secondAnimal.Tipo = Console.ReadLine();
+
+            if (secondAnimal.Tipo.ToUpper() == "CACHORRO") qtdCachorro++;
+            if (secondAnimal.Tipo.ToUpper() == "GATO") qtdGato++;
+            if (secondAnimal.Tipo.ToUpper() == "PEIXE") qtdPeixe++;
+
+
+            //Terceiro
+            Console.Write("Informe o nome do teceiro animal: ");
+            thirdAnimal.Nome = Console.ReadLine();
+
+            Console.Write("Informe o tipo do terceiro animal: ");
+            thirdAnimal.Tipo = Console.ReadLine();
+
+            if (thirdAnimal.Tipo.ToUpper() == "CACHORRO") qtdCachorro++;
+            if (thirdAnimal.Tipo.ToUpper() == "GATO") qtdGato++;
+            if (thirdAnimal.Tipo.ToUpper() == "PEIXE") qtdPeixe++;
+
+            //Quarto
+            Console.Write("Informe o nome do quarto animal: ");
+            fourthAnimal.Nome = Console.ReadLine();
+
+            Console.Write("Informe o tipo do quarto animal: ");
+            fourthAnimal.Tipo = Console.ReadLine();
+
+            if (fourthAnimal.Tipo.ToUpper() == "CACHORRO") qtdCachorro++;
+            if (fourthAnimal.Tipo.ToUpper() == "GATO") qtdGato++;
+            if (fourthAnimal.Tipo.ToUpper() == "PEIXE") qtdPeixe++;
+
+            //Quinto
+            Console.Write("Informe o nome do quinto animal: ");
+            fifthAnimal.Nome = Console.ReadLine();
+
+            Console.Write("Informe o tipo do quinto animal: ");
+            fifthAnimal.Tipo = Console.ReadLine();
+
+            if (fifthAnimal.Tipo.ToUpper() == "CACHORRO") qtdCachorro++;
+            if (fifthAnimal.Tipo.ToUpper() == "GATO") qtdGato++;
+            if (fifthAnimal.Tipo.ToUpper() == "PEIXE") qtdPeixe++;
+
+
+            //Resultado
+            Console.WriteLine("Total de animais");
+            Console.WriteLine($"Cachorros: {qtdCachorro}");
+            Console.WriteLine($"Gatos: {qtdGato}");
+            Console.WriteLine($"Peixes: {qtdPeixe}");
+            Console.ReadKey();
+        }
+    }
+}
